c9096a6b5b10cee7d4ee0715e89b4c5b5efe6c33
Harishwaran1999 <126972758+Harishwaran1999@users.noreply.github.com>
2023-03-05 19:44:11 +0530

    Add files via upload

---
+++ string reversal.py
@@ -0,0 +1,7 @@
+name = input("enter a astring:")
+length = len(name)
+i = 0
+print("original name"," ","reversed name")
+for n in range(-1,-7,-1):
+ print(name[i],"                 ",name[n])
+ i+=1
